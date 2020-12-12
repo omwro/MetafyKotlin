ecfe713b7978ffac097142ac6a1e1da818fa0240
Omer <m.o.erdem@outlook.com>
2020-12-12 16:54:58 +0100

    Login and library fragments

--- app/src/main/java/nl/omererdem/metafy/ui/LibraryFragment.kt
+++ app/src/main/java/nl/omererdem/metafy/ui/LibraryFragment.kt
@@ -1,4 +1,4 @@
-package nl.omererdem.metafy
+package nl.omererdem.metafy.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,25 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import nl.omererdem.metafy.R
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class LibraryFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_library, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
---
+++ app/src/main/java/nl/omererdem/metafy/ui/LoginFragment.kt
@@ -0,0 +1,33 @@
+package nl.omererdem.metafy.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
+import nl.omererdem.metafy.R
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_libraryFragment)
+        }
+    }
+}
